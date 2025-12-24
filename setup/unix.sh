180379ae0fe00c887fcf0f856b2b3a88603f2fb3
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-24 17:55:36 -0500

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -17,7 +17,7 @@ haxelib install hscript 2.4.0 --quiet
 haxelib install tjson 1.4.0 --quiet #for fucking mods system (haxe.Json brokes the modlist)
 haxelib git hxCodec https://github.com/PsychExtendedThings/hxCodec-0.6.3 --quiet #That clearly my repo
 haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet #Just a normal hxcpp
-haxelib git lime https://github.com/HomuHomu833-garbage/lime int-delta --quiet #repo owner helped to fix termux problem
+haxelib install lime --quiet #repo owner helped to fix termux problem
 haxelib install openfl 9.3.3 --quiet #Most stable version of openfl
 haxelib git flxanimate https://github.com/PsychExtendedThings/flxanimate-ex --quiet
 haxelib git funkin.vis https://github.com/beihu235/funkVis-FrequencyFixed main --quiet
